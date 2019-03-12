92ecff92c10824c92dcfe7286b3685e9d5ee59f9
Travis Bateman <41933692+TravyTravMan@users.noreply.github.com>
2019-03-11 21:39:32 -0700

    fix

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 function checkDriverAge() {
 	prompt("What is your age?");
-	if (Number(age) < 18) {
+	} if (Number(age) < 18) {
 		alert("Sorry, you are too young to drive this car");
 	} else if (Number(age) > 18){
 	alert("Powering on")
 	} else if (Number(age) === 18) {
 		alert("Congratulations on your first time driving! Drive Safe!")
-	}	
-} 	
+	}	 	
