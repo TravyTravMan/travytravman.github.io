eee0bdf230505eb87c0d0b4d8fad2f29fc9b57b0
Travis Bateman <41933692+TravyTravMan@users.noreply.github.com>
2019-03-11 21:33:21 -0700

    d

---
+++ carfunction.js
@@ -0,0 +1,10 @@
+function checkDriverAge() {
+	prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too young to drive this car");
+	} else if (Number(age) > 18){
+	alert("Powering on")
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first time driving! Drive Safe!")
+	}	
+} 	
